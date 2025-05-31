901432859e5c6098156254070fc03bd393dc58f4
agung <agungpermadi9@gmail.com>
2025-05-31 22:54:50 +0700

    edit address

--- src/lib/api/AddressApi.js
+++ src/lib/api/AddressApi.js
@@ -1,5 +1,5 @@
 export const createAddress = async (token, id, {street, city, province, country, postalCode}) => {
-    return await fetch(`${import.meta.env.VITE_API_PATH}/contacts/${id}/addresses`, {
+    return fetch(`${import.meta.env.VITE_API_PATH}/contacts/${id}/addresses`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -16,8 +16,37 @@ export const createAddress = async (token, id, {street, city, province, country,
     })
 }
 
+export const updateAddress = async (token, contactId,addressId ,{street, city, province, country, postalCode}) => {
+    return fetch(`${import.meta.env.VITE_API_PATH}/contacts/${contactId}/addresses/${addressId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'X-API-TOKEN': token
+        },
+        body: JSON.stringify({
+            street,
+            city,
+            province,
+            country,
+            postalCode
+        })
+    })
+}
+
 export const addressList = async (token, id) => {
-    return await fetch(`${import.meta.env.VITE_API_PATH}/contacts/${id}/addresses`, {
+    return fetch(`${import.meta.env.VITE_API_PATH}/contacts/${id}/addresses`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'X-API-TOKEN': token
+        }
+    })
+}
+
+export const getAddress = async (token, contactId,addressId) => {
+    return fetch(`${import.meta.env.VITE_API_PATH}/contacts/${contactId}/addresses/${addressId}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
